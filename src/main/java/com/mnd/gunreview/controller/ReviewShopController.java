55933483802182946ce94e6f7c188c9e9abab21f
jieunchae <cj1081@naver.com>
2020-07-10 17:53:19 +0900

    파일 업로드

--- src/main/java/com/mnd/gunreview/controller/ReviewShopController.java
+++ src/main/java/com/mnd/gunreview/controller/ReviewShopController.java
@@ -1,12 +1,23 @@
 package com.mnd.gunreview.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.swing.plaf.multi.MultiProgressBarUI;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +26,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.mnd.gunreview.dto.ReviewShop;
 import com.mnd.gunreview.service.ReviewShopService;
@@ -66,6 +79,43 @@ public class ReviewShopController {
 		}
 		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 	}
+  
+  @ApiOperation(value = "파일을 입력한다. 그리고 DB입력 성공여부에 따라 파일 경로 또는 'fail' 문자열을 반환한다.")
+  @PostMapping("upload")
+	public String doFileUpload(@RequestParam("upload_file") MultipartFile uploadfile, HttpServletRequest request) {	
+		try {
+	  		//웹서비스 경로 지정
+			String root_path = request.getSession().getServletContext().getRealPath("\\");
+			String attach_path = "\\resources\\upload\\";
+			String filename = uploadfile.getOriginalFilename();
+			
+			//System.out.println(root_path+attach_path+filename);
+			
+            FileOutputStream fos = new FileOutputStream(root_path+attach_path+filename);
+            // 파일 저장할 경로 + 파일명을 파라미터로 넣고 fileOutputStream 객체 생성하고
+            InputStream is = uploadfile.getInputStream();
+            // file로 부터 inputStream을 가져온다.
+	  		
+	  		int readCount = 0;
+	        byte[] buffer = new byte[1024];
+	        // 파일을 읽을 크기 만큼의 buffer를 생성하고
+	        // ( 보통 1024, 2048, 4096, 8192 와 같이 배수 형식으로 버퍼의 크기를 잡는 것이 일반적이다.)
+	            
+	        while ((readCount = is.read(buffer)) != -1) {
+	          //  파일에서 가져온 fileInputStream을 설정한 크기 (1024byte) 만큼 읽고
+	                
+	          fos.write(buffer, 0, readCount);
+	          // 위에서 생성한 fileOutputStream 객체에 출력하기를 반복한다
+	        }
+
+			return root_path+attach_path+filename;
+			
+	    }catch (Exception ex) {
+	         throw new RuntimeException("file Save Error");
+	    }
+	}
+
+
   
   @ApiOperation(value = "No에 해당하는 리뷰의 정보를 수정한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@PutMapping("{no}")
