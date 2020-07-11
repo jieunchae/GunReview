26fd3ce3d98b9ddef0f2913f62e530b2bb870968
c9boom7 <dbstjrdk2@naver.com>
2020-07-12 01:40:45 +0900

    Update UserDTO, xml
    user_id,birthday >> Kakao Account 양식으로 변경

--- src/main/java/com/mnd/gunreview/dto/User.java
+++ src/main/java/com/mnd/gunreview/dto/User.java
@@ -1,26 +1,49 @@
 package com.mnd.gunreview.dto;
 
-import java.util.Date;
-
 public class User {
-	private String userid;
-	private Date birth;
-	public String getUserid() {
-		return userid;
+	private String id;
+	private String email;
+	private String age_range;
+	private String birthday;
+	private String gender;
+	
+	public String getId() {
+		return id;
 	}
-	public void setUserid(String userid) {
-		this.userid = userid;
+	public void setId(String id) {
+		this.id = id;
 	}
-	public Date getBirth() {
-		return birth;
+	public String getEmail() {
+		return email;
 	}
-	public void setBirth(Date birth) {
-		this.birth = birth;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	public User(String userid, Date birth) {
+	public String getAge_range() {
+		return age_range;
+	}
+	public void setAge_range(String age_range) {
+		this.age_range = age_range;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public User(String id, String email, String age_range, String birthday, String gender) {
 		super();
-		this.userid = userid;
-		this.birth = birth;
+		this.id = id;
+		this.email = email;
+		this.age_range = age_range;
+		this.birthday = birthday;
+		this.gender = gender;
 	}
 	public User() {
 		super();
