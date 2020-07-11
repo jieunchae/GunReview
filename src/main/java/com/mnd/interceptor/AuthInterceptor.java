b61f20059ec44e5181c7743e88f0d17e528b4a8c
jieunchae <cj1081@naver.com>
2020-07-12 01:51:31 +0900

    AuthInterceptor user반영 전

--- src/main/java/com/mnd/interceptor/AuthInterceptor.java
+++ src/main/java/com/mnd/interceptor/AuthInterceptor.java
@@ -10,7 +10,6 @@ import java.net.URLConnection;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -20,6 +19,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.mnd.gunreview.dto.User;
 
 
 //인증을 위한 인터셉터
@@ -37,17 +37,26 @@ public class AuthInterceptor extends HandlerInterceptorAdapter{
 		//logger.debug("Request URI=====>" + request.getRequestURI());
 		
 		//헤더로부터 토큰을 읽음
-		String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+		//String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+		String token =  "hZmg486N40HlXpqPnCzFIIMhpy69aQS0fZ6RBgorDNQAAAFzPsMTig";
 		//인증과정 수행
 		//1. 토큰으로 부터 kakao 서버에서 userId를 가져옴 - 만약 토큰이 유효하지 않다면 응답 x
-		//2. 토큰으로 가져온 userId가 DB에 있는지 확인
-		//3. 있다면 user정보 넘겨줌
-		//4. 없다면...?
+		String id = getTokenExpired(token);
 		
+		if(id.equals("expired")) {
+			//만료응답
+		}else if(id.equals("type_error")) {
+			//형식에러
+		}else {
+			//2. 토큰으로 가져온 userId가 DB에 있는지 확인
+			
+			//3.없으면 insert
+			getUserInfo(token);
+		}
 		
-		
+		//4. 있다면 user정보 넘겨줌
 		//그 결과를 request attribute로 넘겨준다
-		request.setAttribute("user", user);
+		//request.setAttribute("user", user);
 		return super.preHandle(request, response, handler);
 	}
 
@@ -60,14 +69,15 @@ public class AuthInterceptor extends HandlerInterceptorAdapter{
 	}
 	
 	public String getTokenExpired(String access_token) {
-		String reqURL = "/v1/user/access_token_info";
+		String reqURL = "https://kapi.kakao.com/v1/user/access_token_info";
+		String id = "";
 		try {
 			URL url = new URL(reqURL);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			
 			//요청 Header에 token 포함
-			conn.setRequestProperty("Authorization", "Bearer" + access_token);
+			conn.setRequestProperty("Authorization", "Bearer " + access_token);
 			
 			int responseCode = conn.getResponseCode();
 			System.out.println("responseCode : " + responseCode);
@@ -92,11 +102,63 @@ public class AuthInterceptor extends HandlerInterceptorAdapter{
 				JsonParser parser = new JsonParser();
 				JsonElement element = parser.parse(result);
 				
+				id = element.getAsJsonObject().get("id").getAsString();
 				
+				//확인
+				System.out.println("id : " + id);
+				
+				br.close();
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return id;
+	}
+	
+	public void getUserInfo(String access_token) {
+		String reqURL = "https://kapi.kakao.com/v2/user/me";
+		
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			
+			//요청 Header에 token 포함
+			conn.setRequestProperty("Authorization", "Bearer " + access_token);
+			
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+			
+			//요청을 통해 사용자 정보 얻어옴
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			String result = "";
+				
+			while((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body : " + result);
+				
+			//Json 파싱
+			JsonParser parser = new JsonParser();
+			JsonElement element = parser.parse(result);
+			
+			String id = element.getAsJsonObject().get("id").getAsString();
+			//String email = element.getAsJsonObject().get("email").getAsString();
+			//String age_range = element.getAsJsonObject().get("age_range").getAsString();
+			String birthday = element.getAsJsonObject().get("birthday").getAsString();
+			//String gender = element.getAsJsonObject().get("gender").getAsString();
+				
+			//확인
+			System.out.println("id : " + id);
+			//System.out.println("email : " + email);
+			//System.out.println("age_range : " + age_range);
+			System.out.println("birthday : " + birthday);
+			//System.out.println("gender : " + gender);
+				
+			br.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
