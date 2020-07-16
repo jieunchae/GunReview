b2dd8d66aade4e0d1e603a5dff4abe6a305df3f8
오재민 <average10@naver.com>
2020-07-16 21:36:34 +0900

    crawling update

--- src/main/java/com/mnd/crawling/test.java
+++ src/main/java/com/mnd/crawling/test.java
@@ -17,15 +17,21 @@ public class test {
 
 		JsonReader jsonReader = new JsonReader();
 		StringBuilder URL = new StringBuilder("https://place.map.kakao.com/main/v/");
-		URL.append("17884744");
+//		URL.append("2047692004");
+//		URL.append("143734770");
+		URL.append("1912710954");
+		
 		String getJson = jsonReader.callURL(URL.toString());
 		
 		JsonParser parser = new JsonParser();
 		JsonObject object = (JsonObject) parser.parse(getJson);
 		com.google.gson.JsonArray openHour = (com.google.gson.JsonArray) ((JsonObject)((JsonObject)object.get("basicInfo")).get("openHour")).get("periodList");
 		com.google.gson.JsonArray timeList = (com.google.gson.JsonArray) ((JsonObject) openHour.get(0)).get("timeList");
+		com.google.gson.JsonArray offdayList = (com.google.gson.JsonArray) ((JsonObject)((JsonObject)object.get("basicInfo")).get("openHour")).get("offdayList");
 		
 		StringBuilder sb = new StringBuilder();
+		
+		sb.append("<b>영업시간</b><br>");
 		for(int i=0;i<timeList.size();i++) {
 			JsonObject obj = (JsonObject) timeList.get(i);
 			sb.append(obj.get("dayOfWeek").toString().substring(1, obj.get("dayOfWeek").toString().length()-1) + " : ");
@@ -33,10 +39,21 @@ public class test {
 			sb.append("<br>");
 		}
 		
+		if(offdayList != null) {
+			sb.append("<b>휴무일<b><br>");
+			for(int i=0;i<offdayList.size();i++) {
+				JsonObject obj = (JsonObject) offdayList.get(i);
+				sb.append(obj.get("weekAndDay").toString().substring(1, obj.get("weekAndDay").toString().length()-1) + "<br>");
+			}
+		}
+		
 		System.out.println(sb);
 		
 		
 	}
+	
+	
+	
 /*[
 	{"periodName":"영업기간",
 	 "timeList": [{"timeName":"영업시간","timeSE":"07:00 ~ 24:00","dayOfWeek":"월~금"},{"timeName":"영업시간","timeSE":"08:00 ~ 24:00","dayOfWeek":"토"},{"timeName":"영업시간","timeSE":"08:00 ~ 22:30","dayOfWeek":"일"}]
--- src/main/java/com/mnd/gunreview/controller/ShopController.java
+++ src/main/java/com/mnd/gunreview/controller/ShopController.java
@@ -94,20 +94,31 @@ public class ShopController {
   public String getOpenHour(String shopId) {
 	  JsonReader jsonReader = new JsonReader();
 		StringBuilder URL = new StringBuilder("https://place.map.kakao.com/main/v/");
-		URL.append("1862219579");
+		URL.append(shopId);
 		String getJson = jsonReader.callURL(URL.toString());
 		
 		JsonParser parser = new JsonParser();
 		JsonObject object = (JsonObject) parser.parse(getJson);
 		com.google.gson.JsonArray openHour = (com.google.gson.JsonArray) ((JsonObject)((JsonObject)object.get("basicInfo")).get("openHour")).get("periodList");
 		com.google.gson.JsonArray timeList = (com.google.gson.JsonArray) ((JsonObject) openHour.get(0)).get("timeList");
+		com.google.gson.JsonArray offdayList = (com.google.gson.JsonArray) ((JsonObject)((JsonObject)object.get("basicInfo")).get("openHour")).get("offdayList");
 		
 		StringBuilder sb = new StringBuilder();
+		
+		sb.append("<b>영업시간</b><br>");
 		for(int i=0;i<timeList.size();i++) {
 			JsonObject obj = (JsonObject) timeList.get(i);
 			sb.append(obj.get("dayOfWeek").toString().substring(1, obj.get("dayOfWeek").toString().length()-1) + " : ");
 			sb.append(obj.get("timeSE").toString().substring(1, obj.get("timeSE").toString().length()-1));
 			sb.append("<br>");
+		}
+		
+		if(offdayList != null) {
+			sb.append("<b>휴무일</b><br>");
+			for(int i=0;i<offdayList.size();i++) {
+				JsonObject obj = (JsonObject) offdayList.get(i);
+				sb.append(obj.get("weekAndDay").toString().substring(1, obj.get("weekAndDay").toString().length()-1) + "<br>");
+			}
 		}
 		return sb.toString();
   }
